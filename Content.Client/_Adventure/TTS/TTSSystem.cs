ae0227faf688b7bd62511be2ca1d8fead5181c1f
c4llv07e <igor@c4llv07e.xyz>
2025-01-15 19:08:30 +0300

    Fix errors in client console when tts source moves away from the player

--- Content.Client/_Adventure/TTS/TTSSystem.cs
+++ Content.Client/_Adventure/TTS/TTSSystem.cs
@@ -59,12 +59,14 @@ public sealed class TTSSystem : EntitySystem
 
     private void OnPlayTTS(PlayTTSEvent ev)
     {
-        _sawmill.Verbose($"Play TTS audio {ev.Data.Length} bytes from {ev.SourceUid} entity");
         var audioStream = _audioLoader.LoadAudioWav(new MemoryStream(ev.Data));
 
         var audioParams = AudioParams.Default
             .WithVolume(AdjustVolume(ev.IsWhisper))
             .WithMaxDistance(AdjustDistance(ev.IsWhisper));
+
+        if (ev.SourceUid is { Id: 0 })
+            return;
 
         if (ev.SourceUid != null)
         {
@@ -75,6 +77,7 @@ public sealed class TTSSystem : EntitySystem
         {
             _audio.PlayGlobal(audioStream, audioParams);
         }
+        _sawmill.Verbose($"Play TTS audio {ev.Data.Length} bytes from {ev.SourceUid} entity");
     }
 
     private float AdjustVolume(bool isWhisper)
