7ba0e110bd96b190d239fd654b14ccdbf1456909
NithaRe <corporation.orienta@gmail.com>
2025-01-22 23:20:04 +0500

    удаление антагонистов

--- Content.Client/Lobby/LobbyUIController.cs
+++ Content.Client/Lobby/LobbyUIController.cs
@@ -92,7 +92,6 @@ public sealed partial class LobbyUIController : UIController, IOnStateEntered<Lo
     {
         if (_profileEditor != null)
         {
-            _profileEditor.RefreshAntags();
             _profileEditor.RefreshJobs();
         }
     }
@@ -103,7 +102,6 @@ public sealed partial class LobbyUIController : UIController, IOnStateEntered<Lo
         {
             if (obj.WasModified<AntagPrototype>())
             {
-                _profileEditor.RefreshAntags();
             }
 
             if (obj.WasModified<JobPrototype>() ||
@@ -195,7 +193,6 @@ public sealed partial class LobbyUIController : UIController, IOnStateEntered<Lo
 
     private void RefreshProfileEditor()
     {
-        _profileEditor?.RefreshAntags();
         _profileEditor?.RefreshJobs();
         _profileEditor?.RefreshLoadouts();
     }
